6751c899b3a182f8d4ca968e51adfb65ca628e81
evyatar424-hash <evyatar424@gmail.com>
2026-03-07 17:47:06 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hdshot-il-v3';
+const CACHE = 'hdshot-il-v4';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
